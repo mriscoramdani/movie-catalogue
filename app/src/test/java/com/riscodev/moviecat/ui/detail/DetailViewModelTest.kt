8dc6410c9285cef7606f6ca7909e5582eb40fd43
Risco Ramdani <riscoramdani80@gmail.com>
2021-05-17 03:41:07 +0700

    changelist : - implement unit test for (viewmodel) - re-testing

--- app/src/test/java/com/riscodev/moviecat/ui/detail/DetailViewModelTest.kt
+++ app/src/test/java/com/riscodev/moviecat/ui/detail/DetailViewModelTest.kt
@@ -3,9 +3,12 @@ package com.riscodev.moviecat.ui.detail
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.paging.PagedList
 import com.riscodev.moviecat.data.AppRepository
+import com.riscodev.moviecat.data.source.local.entity.FavoriteEntity
 import com.riscodev.moviecat.data.source.local.entity.MovieEntity
 import com.riscodev.moviecat.data.source.local.entity.ShowEntity
+import com.riscodev.moviecat.ui.main.content.movie.MovieViewModelTest
 import com.riscodev.moviecat.utils.DataDummy
 import com.riscodev.moviecat.vo.Resource
 import org.junit.Test
@@ -39,20 +42,22 @@ class DetailViewModelTest {
     @Mock
     private lateinit var showObserver: Observer<Resource<ShowEntity>>
 
+    @Mock
+    private lateinit var favoriteObserver: Observer<FavoriteEntity>
+
     @Before
     fun setUp() {
         viewModel = DetailViewModel(appRepository)
-        viewModel.setSelectedMovie(movieId)
-        viewModel.setSelectedShow(showId)
     }
 
     @Test
-    fun `setSelectedMovie should be success`() {
+    fun `getMovie should be success`() {
         val expected = MutableLiveData<Resource<MovieEntity>>()
         expected.value = Resource.success(dummyMovie)
 
         `when`(appRepository.getMovie(movieId)).thenReturn(expected)
 
+        viewModel.setSelectedMovie(movieId)
         viewModel.movie.observeForever(movieObserver)
         verify(movieObserver).onChanged(expected.value)
 
@@ -62,18 +67,38 @@ class DetailViewModelTest {
     }
 
     @Test
-    fun `setSelectedShow should be success`() {
+    fun `getShow should be success`() {
         val expected = MutableLiveData<Resource<ShowEntity>>()
         expected.value = Resource.success(dummyShow)
 
         `when`(appRepository.getShow(showId)).thenReturn(expected)
 
+        viewModel.setSelectedShow(showId)
         viewModel.show.observeForever(showObserver)
 
         verify(showObserver).onChanged(expected.value)
 
         val expectedValue = expected.value
         val actualValue = viewModel.show.value
+
+        assertEquals(expectedValue, actualValue)
+    }
+
+    @Test
+    fun `getFavorite should be success`() {
+        val expected = MutableLiveData<FavoriteEntity>()
+        expected.value = DataDummy.generateFavorite()
+
+        val dummyContentId = "1"
+        val dummyContentType = MovieEntity.TYPE
+        `when`(appRepository.getFavorite(dummyContentId, dummyContentType)).thenReturn(expected)
+
+        viewModel.getFavorite(dummyContentId, dummyContentType).observeForever(favoriteObserver)
+
+        verify(favoriteObserver).onChanged(expected.value)
+
+        val expectedValue = expected.value
+        val actualValue = viewModel.getFavorite(dummyContentId, dummyContentType).value
 
         assertEquals(expectedValue, actualValue)
     }
